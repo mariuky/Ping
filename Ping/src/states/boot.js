e480447562577e05d42ffbb6a6a342d1496e52dd
Mario Peña Jimenez <m.penaj.2018@alumnos.urjc.es>
2018-10-10 11:37:40 +0200

    Añadido comentario

--- Ping/src/states/boot.js
+++ Ping/src/states/boot.js
@@ -13,6 +13,7 @@ CatCatcher.bootState.prototype = {
     create: function() {
     	 game.physics.startSystem(Phaser.Physics.ARCADE);
     	 game.state.start('preloadState')
+		 //Prueba Github
     },
 
     update: function() {
